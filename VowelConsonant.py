255828e1bd2c6852eaffecc713f07d7396cd0559
il8677 <is48le50@isdedu.de>
2015-09-03 14:53:32 +0200

    I added a python file called "VowelConsonant.py", In it I printed "Hello world" and then started the process of creating a class, I just finished the class

--- VowelConsonant.py
+++ VowelConsonant.py
@@ -1,4 +1,7 @@
 print("Hello World")
 class hello:
+    greeting = ""
     def __init__(self, greeting):
-        
+        greeting = self.greeting
+    def greet(self):
+        print("The stored greeting is: " + self.greeting)
