10c23d655dae1f8abb56ec6a1844887a15fd2ac3
il8677 <is48le50@isdedu.de>
2015-09-03 15:45:00 +0200

    Completed the program. And the extension

--- VowelConsonant.py
+++ VowelConsonant.py
@@ -1,13 +1,19 @@
-vowels = ["a","e","i","o","u"]
+vowels = "aeiouAEIOU"
+consonants = "bcdfghjklmnpqrstvwxyz"
+consonants+=consonants.upper()
 fvowels = ""
 fcon=""
+fother=""
 userInput = input("Please input sentance to split: ")
 print ("Splitting sentance: " + userInput)
 for char in userInput:
     if char in vowels:
         fvowels += char
+    elif char in consonants:
+        fcon+= char
     else:
-        fcon += char
+        fother += char
 print("Process Completed")
 print("Vowels: " + fvowels)
 print("Consonants: " + fcon)
+print("Other: "+fother)
