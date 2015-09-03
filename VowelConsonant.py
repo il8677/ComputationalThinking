308e95d7705b834f39aa0308266c1598f5ae6ab1
il8677 <is48le50@isdedu.de>
2015-09-03 15:31:50 +0200

    Completed the program.

--- VowelConsonant.py
+++ VowelConsonant.py
@@ -1,7 +1,13 @@
-print("Hello World")
-class hello:
-    greeting = ""
-    def __init__(self, greeting):
-        greeting = self.greeting
-    def greet(self):
-        print("The stored greeting is: " + self.greeting)
+vowels = ["a","e","i","o","u"]
+fvowels = ""
+fcon=""
+userInput = input("Please input sentance to split: ")
+print ("Splitting sentance: " + userInput)
+for char in userInput:
+    if char in vowels:
+        fvowels += char
+    else:
+        fcon += char
+print("Process Completed")
+print("Vowels: " + fvowels)
+print("Consonants: " + fcon)
