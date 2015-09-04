5d401e2bd6936220e937def7265fed3c8c9cecdf
il8677 <is48le50@isdedu.de>
2015-09-04 15:02:56 +0200

    Find alphabet position upgraded, added loop until "Exit" is typed

--- FindPostionInAlphabet.py
+++ FindPostionInAlphabet.py
@@ -1,3 +1,8 @@
-alpha="abcdefghijklmnopqrstuvwxyz"
-userInput = input("What letter position do you want to find? ")
-print(alpha.find(userInput.lower()) + 1)
+done = False
+while not done:
+    alpha="abcdefghijklmnopqrstuvwxyz"
+    userInput = input("What letter position do you want to find? ")
+    if userInput == "exit":
+        done = True
+    else:
+        print(alpha.find(userInput.lower()) + 1)
