401ff2c2f0ee87f74649264ee6d450cf126b6a0f
il8677 <is48le50@isdedu.de>
2015-09-04 07:54:15 +0200

    MOAR COMMENTS!!!!

--- VowelConsonant.py
+++ VowelConsonant.py
@@ -7,11 +7,15 @@ fcon=""
 fother=""
 userInput = input("Please input sentance to split: ")
 print ("Splitting sentance: " + userInput)
+#Iterates through inputed charecters
 for char in userInput:
+    #Checks if the current iterated charecter is a vowel
     if char in vowels:
         fvowels += char
+    #Checks if the current iterated charecter is a consonant
     elif char in consonants:
         fcon+= char
+    #Otherwise
     else:
         fother += char
 print("Process Completed")
