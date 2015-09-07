7725359188ec90f78a0b634687ff24377471bf5b
il8677 <is48le50@isdedu.de>
2015-09-07 14:06:43 +0200

    NUMBER TO LETTER MADE

--- Number to Letter.py
+++ Number to Letter.py
@@ -5,5 +5,12 @@ while not done:
     userInput = input("Input a number and this app will convert it into the letter corresponding to the position in the alphabet: ")
     if userInput == "exit":
         done = True
+    if userInput == "0":
+        print("Dont type 0... thats just mean...")
     else:
-        print(alpha[int(userInput) - 1])
+        try:
+            print(alpha[int(userInput) - 1])
+        except IndexError:
+            print("INDEX ERROR: STRING INDEX OUT OF RANGE (Your number exceeded 26)")
+        except ValueError:
+            print("VALUE ERROR: EXPECTED TYPE INT (Your number was not a number)")
